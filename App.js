1b4f07dc61f87aabf0071e59538d7fbd7105d2e7
paul9590 <paul9590@naver.com>
2023-02-17 07:40:48 +0900

    second

--- App.js
+++ App.js
@@ -1,11 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View, ScrollView, Button, ActivityIndicator } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+const Profile = (props) => {
+  return <View style={{flexDirection: "row"}}> 
+          <Image source={props.source}
+            style={{
+              width: props.width,
+              height: props.height
+            }}/>
+          <Text>{props.title}</Text>
+         </View>
+};
+
+const Btn = (props) => {
+  const [count, setCnt] = useState(0);
+  
+  useEffect(() => {
+    console.log('didMount');
+  }, []);  
+  
+  useEffect(() => {
+    console.log('didUpdate', count);
+    if(count > 10) {
+      setCnt(0);
+    }
+  }, [count]);  
+
+  return <View>
+            <Text onPress={() => setCnt(count + 1)}
+              style={{backgroundColor: "pink",
+                width: 100,
+                height: 100,
+                textAlign: 'center'}}>
+              {count}
+            </Text>
+            {count > 5 && <ActivityIndicator/>}
+         </View>
+  
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>paul2</Text>
       <StatusBar style="auto" />
+      <Text style={styles.text}>paul2</Text>
+      <Image source={require('./assets/img.png')}/>
+      <Button title="안녕?"
+        onPress={() => {
+          console.log("D");
+        }}
+      /> 
+
+      <Profile source={require('./assets/img.png')} 
+                title="d" width={100} height={100}/>
+
+      <Profile source={require('./assets/img.png')}
+                title="d" width={25} height={25}/>
+
+      <Btn/>
     </View>
   );
 }
@@ -16,5 +69,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  text: {
+    fontSize: 20,
+    fontWeight: "bold"
   },
 });
